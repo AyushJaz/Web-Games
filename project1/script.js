d4251bea8c52af35209f95ec6605af98259892a7
AyushJaz <ajayush2004@gmail.com>
2024-06-20 11:29:31 +0530

    final submition my edition better code

--- project1/script.js
+++ project1/script.js
@@ -8,22 +8,61 @@ playerImage.src = "./shadow_dog.png";
 //  dimention of the sample picture is 6876px * 5230px devide equaly in 12 colums and 10 rows
 let breath = 523;//total height divded by no of rows 
 let length = 575; //total width devided ny no of colums +2 for margin error
-let frameX = 0;
-let frameY = 0;
 let gameframe = 0;
-let rate = 3;
-function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position =Math.floor(gameframe/rate)%6;
-    frameX=length*position;
-
-    ctx.drawImage(playerImage, frameX, frameY * breath, length, breath, 0, 0, length, breath)
-    if (gameframe % rate == 0) 
+let rate = 5;
+let animations = [
     {
-        if (frameX < 5)
-            frameX++;
-        else
-            frameX = 0
+        name: 'ideal',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },  {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },  {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'get',
+        frames: 4,
+    },
+]
+let i=0,k=0;
+
+let state=document.getElementById('dog');
+state.addEventListener('click',()=>{
+    k=state.value;
+})
+
+
+function animate() {
+    if(gameframe % rate == 0){
+        ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    ctx.drawImage(playerImage,(i%((animations[k].frames)-1))*length,k*breath,length,breath,0,0,length,breath);
+    i++;
     }
     gameframe++;
     requestAnimationFrame(animate);
